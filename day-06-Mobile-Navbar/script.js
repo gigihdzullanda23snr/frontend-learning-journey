d068c3cd00f617cb4e912d13d1ae7954bdf06ae0
Gigih <gigihdzullanda23@gmail.com>
2026-01-30 09:18:03 +0800

    Update script.js

--- day-06-Mobile-Navbar/script.js
+++ day-06-Mobile-Navbar/script.js
@@ -1,1 +1,8 @@
+  <script>
+      const hamburger=document.getElementById("hamburger");
+      const menu=document.getElementById("menu");
 
+      hamburger.addEventListener("click",() =>
+    {menu.classList.toggle("show");
+    });
+  </script>
