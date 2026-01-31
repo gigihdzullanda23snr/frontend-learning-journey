7e1dc571a0055ee00246c6d0d2a9726984ce8533
Gigih <gigihdzullanda23@gmail.com>
2026-01-31 19:41:49 +0800

    Update script.js

--- day-07-Real-Portfolio-feel/script.js
+++ day-07-Real-Portfolio-feel/script.js
@@ -1,1 +1,11 @@
+window.addEventListener("scroll",()=>{
+    document.querySelector(".navbar")
+      .classList.toggle("scrolled",window.scrollY > 10);
+});
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
 
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  hamburger.classList.toggle("active");
+});
