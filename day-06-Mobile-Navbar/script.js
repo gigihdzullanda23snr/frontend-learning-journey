3ad6bbda26a7218705095d6749282eb60d197301
Gigih <gigihdzullanda23@gmail.com>
2026-01-31 17:04:45 +0800

    Update script.js

--- day-06-Mobile-Navbar/script.js
+++ day-06-Mobile-Navbar/script.js
@@ -1,3 +1,7 @@
+window.addEventListener("scroll",()=>{
+    document.querySelector(".navbar")
+      .classList.toggle("scrolled",window.scrollY > 10);
+});
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 
