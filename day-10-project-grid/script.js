b924b454185c969997e8ce5be58194cae0127218
Gigih <gigihdzullanda23@gmail.com>
2026-03-16 04:10:48 +0800

    Fix button click event handling for active class

--- day-10-project-grid/script.js
+++ day-10-project-grid/script.js
@@ -5,6 +5,13 @@ buttons.forEach(button => {
 
 button.addEventListener("click", () => {
 
+button.forEach(btn => btn.classList.remove("active"));
+
+button.classList.add("active");
+});
+});
+
+
 const filter = button.getAttribute("data-filter");
 
 cards.forEach(card => {
