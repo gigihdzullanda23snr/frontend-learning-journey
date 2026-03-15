f809b7f8eedcb39735b5e0791c366b3f40f43dff
Gigih <gigihdzullanda23@gmail.com>
2026-03-16 00:16:32 +0800

    Update script.js

--- day-08-DOM/script.js
+++ day-08-DOM/script.js
@@ -1,1 +1,7 @@
 
+const button = document.getElementById("btn");
+const text = document.getElementById("text");
+
+button.addEventListener("click", function() {
+    text.innerHTML = "Keren! Kamu baru saja menjalankan JavaScript 🚀";
+});
